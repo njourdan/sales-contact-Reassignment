2c54162c38173868603d45d8212f310c29cf4b3c
njourdan <njourdan@sigmaequipment.com>
2024-09-10 15:25:18 -0500

    cleanup

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import {parse} from 'csv'
 import fs from 'fs'
-
+const contactExport = []
 fs.createReadStream('src/Contacts.csv')
 .pipe(
 parse({
@@ -8,11 +8,11 @@ parse({
         trim: true
 }))
 .on('data', (row) => {
-    console.log(row);
+    contactExport.push(row)
   })
   .on('error', (err) => {
-    console.error(err);
+    console.error(err)
   })
   .on('end', () => {
-    console.log(data.length);
+    console.log(contactExport.length)
   });
