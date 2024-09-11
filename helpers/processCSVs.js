5f4036bd357cce72297f53f15a6137425fd9aa17
njourdan <njourdan@sigmaequipment.com>
2024-09-11 11:42:35 -0500

    semi-polished just need to write csv writing code at the end of main function

--- helpers/processCSVs.js
+++ helpers/processCSVs.js
@@ -13,9 +13,6 @@ export async function processCSVs(filename) {
           })
         )
         .on('data', (row) => {
-          if (filename == 'reallocate') {
-            // console.log(row);
-          }
           data.push(row);
         })
         .on('error', (err) => {
@@ -23,7 +20,6 @@ export async function processCSVs(filename) {
           reject(err); // Reject the promise in case of error
         })
         .on('end', () => {
-          console.log(`${filename} CSV processed with ${data.length} rows`);
           resolve(data); // Resolve the promise with the data array
         });
     });
@@ -33,4 +29,36 @@ export async function processContacts() {
     const allContacts = await processCSVs('all');
     const reallocate = await processCSVs('reallocate');
     return { allContacts, reallocate };
+  }
+export function dedupeAndTransform(arr, validIds) {
+/* thus function takes an array of contacts,  and an array of validids, iterates through the array 
+first seeing if the contact is owned by a valid owner, then checks if there is a count object for that owner
+if there is none it creates an hash object for the owner, and sets the count to 1, if it finds an existing one then it iterates
+the end result is an array that looks something like this
+[ { user: 1871085, count: 1 }, { user: 1871113, count: 1 } ]
+  next if finds the max, ie the object in the array with the highest count, then returns it.
+  if there are multiple it returns multiple
+  */
+    const counts = {};
+  
+    arr.forEach(contact => {
+      let ownerID = parseInt(contact.ContactOwner.split(";")[0])
+      if (validIds.includes(ownerID)) {
+        if (counts[ownerID]) {
+          counts[ownerID]++;
+        } else {
+          counts[ownerID] = 1;
+        }
+      }
+    });
+  
+    const result = Object.keys(counts).map(key => ({
+      user: parseInt(key),
+      count: counts[key]
+    }));
+  
+    const maxCount = Math.max(...result.map(obj => obj.count));
+    const largest = result.filter(obj => obj.count === maxCount);
+  
+    return largest;
   }
--- main.js
+++ main.js
@@ -1,19 +1,50 @@
-import { processCSVs , processContacts } from './helpers/processCSVs.js';
-
+import { processCSVs , processContacts, dedupeAndTransform } from './helpers/processCSVs.js';
+const saleUserIDs =[2109229,2197147,1930596,1871085,2012130,2104545,2200280,1871087,1871113,1871094,1871088]
 async function main() {
+  const winner = []
+  const tie = []
+  const none = []
   try {
-    const { allContacts, reallocate } = await processContacts();
 
-    console.log(reallocate[5800].OrganizationRecordId)
-    let matches = allContacts.filter(contact => contact.OrganizationRecordId === reallocate[5800].OrganizationRecordId)
-    console.log(matches)
-    // for (let i = 0; i < reallocate.length; i++) {
-    //   console.log("Iteration: " + i);
-    // }
+    const { allContacts, reallocate } = await processContacts();
+    for (let i = 0; i < reallocate.length; i++) {
+      console.log("Iteration: " + i);
+    
+    let matches = allContacts.filter(contact => contact.OrganizationRecordId === reallocate[i].OrganizationRecordId)
+    let result = await dedupeAndTransform(matches,saleUserIDs)
+    // console.log(reallocate[i].ContactOwner)
+    if(result.length == 0){
+      none.push(reallocate[i])
+    }else if(result.length == 1){
+      reallocate[i].ContactOwner = result[0].user
+      // console.log(reallocate[i].ContactOwner)
+      winner.push(reallocate[i])
+    }else if(result.length > 1){
+      console.log(result)
+    }
+    }
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   } catch (error) {
-    console.error('Error processing CSV files:', error);
+    console.error(`Error in main: ${error}`);
   }
+  console.log(winner.length)
 }
 
 main();
